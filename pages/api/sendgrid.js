9193e19c28aaae23379c7620f791d6db81264796
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2022-05-03 19:59:57 -0700

    edit header

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -14,20 +14,19 @@ async function sendEmail(req, res) {
       <html lang="en">
       <head>
         <meta charset="utf-8">
-
-        <title>The HTML5 Herald</title>
-        <meta name="description" content="The HTML5 Herald">
+        <title>New Member Application</title>
+        <meta name="description" content="New Member Application">
         <meta name="author" content="SitePoint">
-      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-
+        <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
         <link rel="stylesheet" href="css/styles.css?v=1.0">
-
       </head>
 
       <body>
         <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">
               </div>
               <div class="container" style="margin-left: 20px;margin-right: 20px;">
+              <h3>You've got a new member application! NOTE: this message does not confirm they have paid dues. </h3>
+
               <h3>You've got a new mail from ${req.body.fullname}, their email is: ✉️ ${req.body.email} </h3>
               <div style="font-size: 16px;">
               <p>Message:</p>
