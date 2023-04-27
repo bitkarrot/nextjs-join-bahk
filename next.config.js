2c443ab377e255fb63d8a0be465800cd5a3ffd50
eriknewland <114263701+eriknewland@users.noreply.github.com>
2023-04-27 10:33:43 -0700

    adjusting env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env:{
+    API_KEY: process.env.ADMIN_API_KEY,
+    INVOICE_API_KEY: process.env.INVOICE_API_KEY,
+    ON_CHAIN_WALLET: process.env.ON_CHAIN_WALLET,
+    LNBITS_WALLET: process.env.LNBITS_WALLET,
+    LNBITS_BASE_URL: process.env.LNBITS_BASE_URL,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  }
 }
 
 module.exports = nextConfig
